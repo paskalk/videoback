09603158b734aca2db46aa21610406c3312c96d9
Pac <paskalk@gmail.com>
2020-08-16 23:02:02 +0200

    Added CORS policy

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# videoback
+# video backend
 Backend api using django &amp; vagrant
+
+Follow these steps:
+
+```
+1. vagrant up
+2. vagrant ssh
+3. cd /vagrant
+4. source ~/env/bin/activate
+5. python manage.py runserver 0.0.0.0:8000
--- videos_backend/settings.py
+++ videos_backend/settings.py
@@ -39,18 +39,23 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'videos_api'
+    'corsheaders',
+    'videos_api',
+    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'videos_backend.urls'
 
@@ -121,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
